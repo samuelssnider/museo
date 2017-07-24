110247d68cc44cab51159c82e6d88ab508548bd2
samuelssnider <sam_redins@yahoo.com>
2017-07-24 09:53:53 -0600

    working

--- lib/curator.rb
+++ lib/curator.rb
@@ -1,3 +1,5 @@
+require './lib/museum'
+require 'pry'
 class Curator
   attr_reader :museums,
               :photographs,
@@ -8,5 +10,18 @@ class Curator
     @artists = []
   end
 
-  
+  def add_museum(hash)
+    other_hash = {id: (@museums.count + 1)}
+    final = hash.merge(other_hash)
+    museum = Museum.new(final)
+    @museums << museum
+  end
+
+  def add_artist(hash)
+    final = hash.merge({id: (@artists.count + 1)})
+    artist = Artist.new(final)
+    @artists << artist
+  end
+
+
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -2,6 +2,7 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/curator'
+require './lib/museum'
 
 class CuratorTest < Minitest::Test
   def test_it_can_be_initailized
@@ -16,5 +17,27 @@ class CuratorTest < Minitest::Test
     assert c.photographs.empty?
     assert c.artists.empty?
   end
+
+  def test_we_can_add_museums
+    c = Curator.new
+    c.add_museum({name: "MoMA"})
+    assert_equal 1, c.museums.length
+    assert_equal "MoMA", c.museums[0].name
+    assert_equal 1, c.museums.first.id
+  end
+
+  def test_we_can_add_artists
+    c = Curator.new
+    c.add_artist({name: "Ansel Adams", born: 1902, died: 1984, country: "United States"})
+    assert_equal 1, c.artists.count
+    assert_equal 1, c.artists.first.id
+    assert_equal "Ansel Adams", c.artists.first.name
+    assert_equal 1902, c.artists.first.born
+    assert_equal 1984, c.artists.first.died
+    assert_equal "United States", c.artists.first.country
+  end
+
+
+
 
 end
